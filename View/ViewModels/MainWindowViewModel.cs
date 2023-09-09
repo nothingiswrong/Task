a423e77ae8808c634ca91f2326cee2bba647eebb
Fyodor <betterideass8810@gmail.com>
2023-09-09 23:23:02 +0300

    added plot

--- View/ViewModels/MainWindowViewModel.cs
+++ View/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace AvaloniaApplication1.ViewModels;
 
-public class MainWindowViewModel : INotifyPropertyChanged
+public class MainWindowViewModel : ObservableObject, INotifyPropertyChanged
 {
     private int _num = 0;
     private int _start = 0;
@@ -29,16 +32,25 @@ public class MainWindowViewModel : INotifyPropertyChanged
         set => _step = int.TryParse(value, out var x) ? x : 0;
     }
 
-
+    //Not implemented yet
     public void CalcButtonOnClick()
     {
         Console.WriteLine("It works!");
     }
 
+    //Plot
+    
+    public ISeries[] Series { get; set; } 
+        = new ISeries[]
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
 
-
-
-
+    
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
