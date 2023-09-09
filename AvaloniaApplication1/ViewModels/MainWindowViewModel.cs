38748182f8144e5ff4dd2ba2719d44b5e6ac0a9d
fyodor101 <betterifeass8810@gmail.com>
2023-09-09 12:00:17 +0300

    fbwgeg

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace AvaloniaApplication1.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    private int StartValue = 0;
+    private int NumValue = 0;
+    private int StepValue = 0;
+    
+
+    public int Start
+    {
+        get => StartValue;
+        set
+        {
+            if (!int.TryParse(value.ToString(), out StartValue)) StartValue = 0;
+        }
+    }
+    
+    public int Num
+    {
+        get => StartValue;
+        set
+        {
+            if (!int.TryParse(value.ToString(), out NumValue)) NumValue = 0;
+        }
+    }
+    
+    public int Step
+    {
+        get => StartValue;
+        set
+        {
+            if (!int.TryParse(value.ToString(), out StepValue)) StepValue = 0;
+        }
+    }
+    
+    
+    
+}
---
+++ View/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+using ReactiveUI;
+
+namespace View.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    public string Content = "Content";
+}
