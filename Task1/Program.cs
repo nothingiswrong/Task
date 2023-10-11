99c7ccd68c62ad69fb48a99c89e9380eb75de70a
fyodor101 <betterifeass8810@gmail.com>
2023-10-11 10:14:56 +0300

    res

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Task1;
 
-Console.WriteLine("Hello, World!");
+var arr1 = new int[] {4, 5, 76};
+var arr2 = new int[] {3, 6, 33,234,4444};
+foreach (var i in new TaskMaster().N7(arr1, arr2)) Console.Write($"{i} ");
